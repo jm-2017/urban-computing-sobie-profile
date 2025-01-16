4a13ea7ee493cac9744731ef276837c05cdd08ac
Martinez <jmartinez4@una.edu>
2025-01-16 16:43:16 -0600

    hello

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = procesd.rnv.PORT || 3000;
+
+// endpoint, middleware(s)
+app.get('/', function (req, res) {
+    res.send('Hello Express')
+})
+
+app.listen(port, ()=> console.log(`server is running on... ${port}`
+        )
+    );
