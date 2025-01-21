bc5a95fd1d2bfab1401272395dbc6c042841068e
Martinez <jmartinez4@una.edu>
2025-01-21 16:49:05 -0600

    hell0

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000;
 
+app.use(express.static(__dirname + '/public'))
+
 // endpoint, middleware(s)
 app.get('/', function (req, res) {
-    res.send('Hello Express')
+    res.sendFile('index.html')
 })
+
+// endpoint, middleware(s)
+app.get('/helloRender', function (req, res) {
+    res.send('Hello Express from real world')
+})
+
 
 app.listen(port, ()=> console.log(`server is running on... ${port}`
         )
