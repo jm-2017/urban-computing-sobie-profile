7a097e8ada66f5d5fb8467dc9315ec719fc04125
Martinez <jmartinez4@una.edu>
2025-01-21 16:50:33 -0600

    hello

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000;
 
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'))
 
 // endpoint, middleware(s)
@@ -9,9 +10,20 @@ app.get('/', function (req, res) {
     res.sendFile('index.html')
 })
 
+app.get('/ejs', function (req, res) {
+    res.render(' words, {pageTitle : my cool ejs page'})
+})
+app.get('/nodemon', function (req, res) {
+    res.sendFile('no kill')
+})
+
+app.get('/ejs', function (req, res) {
+    res.render('words')
+})
+
 // endpoint, middleware(s)
 app.get('/helloRender', function (req, res) {
-    res.send('Hello Express from real world')
+    res.send('Hello Express from real world<br><a href="/"> back to home</a>')
 })
 
 
